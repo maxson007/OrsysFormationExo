9a1ccb43970e4e7a33d5debb0b5f66795b463a2f
usera <usera@localhost.localdomain>
2020-02-18 09:07:18 +0100

    modification de la fonction liste remarque formateur

--- Listes.py
+++ Listes.py
@@ -1,13 +1,12 @@
 
 listeInitiale=[1,43,8,-23,12,-67,42,8,21,-12,-42,-8,21,-12,-42,-61,8,17,42]
 nouvelleListe=[]
-
+listeSansValeurNegatif=[]
 #print(dir(listeInitiale))
 for value in listeInitiale:
-    index=listeInitiale.index(value)
-    listeInitiale[index]= abs(value)
+    listeSansValeurNegatif.append(abs(value))
 
-for value in listeInitiale:
+for value in listeSansValeurNegatif:
     if value not in nouvelleListe:
         nouvelleListe.append(abs(value))
 
