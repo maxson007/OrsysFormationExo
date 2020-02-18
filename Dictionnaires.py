2a8400342988fd7a72ce08f8dd19e8562ed26a49
usera <usera@localhost.localdomain>
2020-02-18 12:09:00 +0100

    implementation du dico travaux

--- Dictionnaires.py
+++ Dictionnaires.py
@@ -1,6 +1,10 @@
 dico={'r':3, 'a':0,'y':5, 'z':1, 't':4, 'e':2}
 
 
-sorted(dico)
+dicoKeys=dico.keys()
+dicoValues=dico.values()
 
-print(dico)
+for key in sorted(dicoKeys):
+    for value in sorted(dicoValues):
+        if dico.get(key)==value:
+            print("%s=>%s" %(key, dico.get(key)))
