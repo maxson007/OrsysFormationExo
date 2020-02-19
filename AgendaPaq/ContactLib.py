0fdf54a4eab723e19c59390fc3b52df8b6d1ed57
usera <usera@localhost.localdomain>
2020-02-19 16:01:22 +0100

    Agenda 5

--- AgendaPaq/ContactLib.py
+++ AgendaPaq/ContactLib.py
@@ -3,37 +3,60 @@ import shelve
 
 class Contact():
     """ Class contact pour creer des objet de type contact"""
-    def __init__(self,nom, prenom, tel, email):
-        self.nom=nom
-        self.prenom=prenom
-        self.tel=tel
-        self.email=email
+
+    def __init__(self, nom, prenom, tel, email):
+        self.nom = nom
+        self.prenom = prenom
+        self.tel = tel
+        self.email = email
+
     def __str__(self):
         return '{0[0]:<20} {0[1]:<20} {0[2]:<10} {0[2]:<20}'.format((self.nom, self.prenom, self.tel, self.email))
 
+#Exception de saissie invalide
+class SaisieInvalide(Exception):
+    def __init__(self, valeur):
+        self.valeur=valeur
+
+    def __str__(self):
+        return repr(self.valeur)
+
+#Class Agenda
 class Agenda():
-    instance= None
+    """Agenda class agenda"""
+    instance = None
     agenda = shelve.open("agenda.dat")
 
     def __new__(cls, *args, **kwargs):
         if cls.instance is None:
-            cls.instance=object.__new__(cls)
-            return  cls.instance
+            cls.instance = object.__new__(cls)
+            return cls.instance
 
     def afficher_titre(self):
-         print("Mon Agenda")
+        print("Mon Agenda")
 
     def afficher_menu(self):
         print("Taper 1: Pour ajouter un nouveau contact.")
         print("Taper 2: Pour afficher la liste.")
         print("Taper 3: Pour quitter.")
 
+    def is_valid_name(self, name):
+        name=str(name)
+        name=name.replace(" ","").replace("-","")
+        if not name.isalpha():
+            return False
+        return True
+
     def ajouer_contact(self):
         nom = input("taper le nom: ")
+        if not self.instance.is_valid_name(nom):
+            raise SaisieInvalide("Le nom : "+nom+" n'est pas valide")
         prenom = input("taper le prenom: ")
+        if not self.instance.is_valid_name(prenom):
+            raise SaisieInvalide("Le nom : "+prenom+" n'est pas valide")
         tel = input("taper le tel: ")
         email = input("taper le email: ")
-        self.instance.agenda[tel]=Contact(nom, prenom, tel, email)
+        self.instance.agenda[tel] = Contact(nom, prenom, tel, email)
 
     def voir_contact(self):
         print("====" * 20)
